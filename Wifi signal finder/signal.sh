7b610ca5ac1338e05967b5382b659255ee3aff8d
Luk22sand <77746493+Luk22sand@users.noreply.github.com>
2022-08-02 10:27:30 +0200

    Add files via upload

---
+++ Wifi signal finder/signal.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+#wavemon
+
+#usues by default wlan0 -- F7 to change settings
+echo
+echo "------------------------------------------------------------------------"
+echo
+echo "+ This is native Linux tool giving option to measure WiFi signal strength +"
+echo
+sleep 2
+
+sudo wavemon
